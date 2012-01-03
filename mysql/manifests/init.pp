590fe45bd0b17cfb887bf72a494c791c910d7799
John Wiegley <johnw@newartisans.com>
2012-01-03 02:48:55 -0600

    Started creating a redmine module

--- mysql/manifests/init.pp
+++ mysql/manifests/init.pp
@@ -52,3 +52,42 @@ class mysql
   #  args => "mysql!mysql";
   #}
 }
+
+define mysql::gem()
+{
+  exec { "manually install mysql gem":
+    path    => "/usr/sbin:/usr/bin/:/sbin:/bin",
+    user    => root,
+    command => "gem install mysql -- --with-mysql-dir=/usr/mysql --with-mysql-lib=/usr/mysql/lib --with-mysql-include=/usr/mysql/include",
+    unless  => "gem list mysql | grep ^mysql",
+    require => [ Package[$packages], Package[$devel_pkgs] ];
+  }
+}
+
+define mysql::database($user, $passwd, $host = "localhost")
+{
+  exec { "create MySQL user $user":
+    path    => "/usr/sbin:/usr/bin/:/sbin:/bin",
+    user    => root,
+    command => "sleep 30; mysql mysql -e \"CREATE USER $user@$host IDENTIFIED BY '$passwd';\"",
+    unless  => "mysql mysql -e \"SELECT user FROM user WHERE user='$user'\" | grep $user",
+    require => Service[mysqld];
+  }
+
+  exec { "create MySQL database $title":
+    path    => "/usr/sbin:/usr/bin/:/sbin:/bin",
+    user    => root,
+    command => "mysql -e 'CREATE DATABASE $title'",
+    unless  => "mysql -e 'SHOW DATABASES' | grep $title",
+    require => Exec["create MySQL user $user"];
+  }
+
+  exec { "grant MySQL user $user":
+    path        => "/usr/sbin:/usr/bin/:/sbin:/bin",
+    user        => root,
+    command     => "mysql -e \"GRANT ALL PRIVILEGES ON $title.* TO $user@$host IDENTIFIED BY '$passwd'\"",
+    refreshonly => true,
+    subscribe   => Exec["create MySQL database $title"],
+    require     => Exec["create MySQL database $title"];
+  }
+}
